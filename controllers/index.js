25ddc024144ccbd7debb56fa5553fa3d02946a99
unknown <peters0470@gmail.com>
2021-01-12 14:36:20 -0600

    added login and logout code

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,4 @@ const homeRoutes = require('./home-routes.js');
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-router.use((req, res) => {
-  res.status(404).end();
-});
-
 module.exports = router;
